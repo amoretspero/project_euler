bc41f8832bf021a7dd4d9780ff25abba842065bb
amoretspero <amoretspero@gmail.com>
2015-01-16 01:57:22 +0900

    Problem 16 Solved

---
+++ prob16/prob16.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.Diagnostics
+open System.Collections.Generic
+open System.Numerics
+
+let sw = Stopwatch()
+sw.Start()
+
+let mutable init = BigInteger(1)
+for i=1 to 1000 do
+    init <- init * BigInteger(2)
+
+let answer_str = init.ToString()
+let mutable answer = 0
+for i=0 to answer_str.Length-1 do
+    answer <- answer + (Convert.ToInt32(Convert.ToString(answer_str.[i])))
+
+sw.Stop()
+printfn "Answer : %d" answer
+printfn "Elapsed Time : %f" sw.Elapsed.TotalMilliseconds
