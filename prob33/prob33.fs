840ff61657c1a6f12b8c593a2c1d05278e4ae02e
amoretspero <amoretspero@gmail.com>
2015-02-05 02:49:46 +0900

    Practicing git branch. Started solving problem 33.

---
+++ prob33/prob33.fs
@@ -0,0 +1,10 @@
+﻿open System
+open System.Collections
+open System.Diagnostics
+open System.IO
+open System.Linq
+open System.Numerics
+open System.Collections.Generic
+
+let sw = Stopwatch()
+sw.Start()
