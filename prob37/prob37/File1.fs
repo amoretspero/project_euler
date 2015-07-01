260709c7722bbafd0caba4ed09271a7ff1f442e6
Jiung Hahm <amoretspero@gmail.com>
2015-07-01 22:09:57 +0900

    Solving prob37-prob39

---
+++ prob37/prob37/File1.fs
@@ -0,0 +1,66 @@
+﻿module File1
+
+open System
+open System.Collections
+open System.Collections.Generic
+open System.Diagnostics
+open System.IO
+open System.Linq
+open System.Numerics
+
+let prime_set = new HashSet<int>()
+
+let mutable prime_set_end = 0
+
+let prime_gen (n : int) =
+    prime_set_end <- n
+    for i=2 to n do
+        if i=2 then
+            prime_set.Add(i) |> ignore
+        else if i%2<>0 then
+            let mutable res = true
+            for elem in prime_set do
+                if i%elem = 0 then
+                    res <- false
+                else
+                    res <- res
+            if res then
+                prime_set.Add(i) |> ignore
+
+let rec check_truncate_prime (n : int) =
+    if prime_set_end < n then
+        prime_gen(n)
+    if prime_set.Contains(n) && n > 7 then
+        let mutable res = true
+        let digit = n.ToString().Length
+        for i=0 to digit-2 do
+            let left_trunc = n/(Convert.ToInt32(System.Math.Pow(float 10, float (digit - 1 - i))))
+            let right_trunc = n%(Convert.ToInt32(System.Math.Pow(float 10, float (digit - 1 - i))))
+            //printfn "left_trunc : %d, right_trunc : %d" left_trunc right_trunc
+            if res then
+                res <- (prime_set.Contains(left_trunc)) && (prime_set.Contains(right_trunc))
+            else
+                res <- res
+        res
+    else
+        false
+
+let sw = new Stopwatch()
+let mutable prime_cnt = 0
+let mutable cnt = 1
+let mutable num_of_truncate_primes = -1
+
+printfn "Insert number of truncate primes to find(MAX : 11)"
+num_of_truncate_primes <- Convert.ToInt32(Console.ReadLine())
+if num_of_truncate_primes > 11 then
+    printfn "Adjusting your input to 11"
+    num_of_truncate_primes <- 11
+
+sw.Start()
+while prime_cnt < num_of_truncate_primes do
+    if (check_truncate_prime cnt) then
+        printfn "Found truncate prime : %d - Elapsed Time : %d(ms)" cnt sw.ElapsedMilliseconds
+        prime_cnt <- prime_cnt + 1
+    cnt <- cnt + 1
+sw.Stop()
+printfn "Found %d truncate primes - Elapsed Time : %d(ms)" num_of_truncate_primes sw.ElapsedMilliseconds
---
+++ prob39/prob39/File1.fs
@@ -0,0 +1,39 @@
+﻿module File1
+
+open System
+open System.Collections
+open System.Collections.Generic
+open System.Diagnostics
+open System.IO
+open System.Linq
+open System.Numerics
+
+let mutable p_start = -1
+let mutable p_end = -1
+printfn "Insert p_start : (MIN : 12)"
+p_start <- Convert.ToInt32(Console.ReadLine())
+printfn "Insert p_end : (MAX : 1000)"
+p_end <- Convert.ToInt32(Console.ReadLine())
+
+let mutable ans = 0
+let mutable ans_p = 0
+let sw = new Stopwatch()
+sw.Start()
+for cnt=p_start to p_end do
+    let square_lst = List.map (fun i -> i * i) [1 .. cnt-2]
+    let mutable res = 0
+    for i=1 to cnt-3 do
+        for j=i to cnt-3 do
+            let k = cnt - i - j
+            if k < 0 || k < i || k < j then
+                res <- res
+            else
+                if (i*i + j*j = k*k) then
+                    printfn "Solution found for p = %d, i=%d, j=%d, k=%d" cnt i j k
+                    res <- res + 1
+    //printfn "Number of solutions for p = %d is : %d" cnt res
+    if res > ans then
+        ans <- res
+        ans_p <- cnt
+sw.Stop()
+printfn "Maximum number of solutions (at p = %d) : %d - Elapsed Time : %d" ans_p ans sw.ElapsedMilliseconds
