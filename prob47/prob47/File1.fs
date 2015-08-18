a13619ae5788e42d938d17d028fac71d451d52e1
Jiung Hahm <amoretspero@gmail.com>
2015-08-18 21:57:30 +0900

    Solving Prob47 - Need to get rid of elementAt methods as much as possible.

--- prob47/prob47/File1.fs
+++ prob47/prob47/File1.fs
@@ -10,86 +10,106 @@ open System.Numerics
 
 open prime_funcs
 
-type prime_factor (pow_base : int, pow_exp : int) =
-    /// pow_base : Base of power.
-    member pf.pow_base = pow_base
-    /// pow_exp : Exponent of power.
-    member pf.pow_exp = pow_exp
-    /// ToString() : Gets the usual power notation (n ^ m).
-    override pf.ToString() = pf.pow_base.ToString() + "^" + pf.pow_exp.ToString()
+exception EMPTY_LIST
 
 let prime_set = new HashSet<int>()
 let prime_set_end = ref 0
 
+let mutable prime_num_insert = -1
+printfn "Enter prime_num_insert : "
+prime_num_insert <- System.Convert.ToInt32(System.Console.ReadLine())
+
+let mutable prime_factor_insert = -1
+printfn "Enter prime_factor_insert : "
+prime_factor_insert <- System.Convert.ToInt32(System.Console.ReadLine())
+
+let rec print_list (lst : prime_factor list) =
+    match lst with
+    [] -> raise EMPTY_LIST
+    | [ elem ] -> printfn "%s " (elem.ToString())
+    | h :: t ->
+        printf "%s " (h.ToString())
+        print_list t
+
+let rec prime_factor_mult (n : int) (lst : prime_factor list) =
+    match lst with
+    [] -> raise EMPTY_LIST
+    | [elem] -> n * (int (System.Math.Pow(float elem.basis, float elem.exp)))
+    | h :: t ->
+        prime_factor_mult (n * (int (System.Math.Pow(float h.basis, float h.exp)))) t
+
+let check_prime_factor (n : int) (lst : prime_factor list) =
+    let pf_res = prime_factor_mult 1 lst
+    if n <> pf_res then false else true
+
 let factored_set = new System.Collections.Hashtable()
 
+let prime_factor_adder (factor : int) (exp : int) (lst : prime_factor list) =
+    let lst_copied = List.foldBack (fun elem acc -> elem::acc) lst []
+    let mutable cnt = 0
+    let mutable loop_break = false
+    while (cnt < lst_copied.Length) && (not loop_break) do
+        if (lst_copied.[cnt].basis = factor) then
+            lst_copied.[cnt].exp <- lst_copied.[cnt].exp + exp
+            loop_break <- true
+        cnt <- cnt + 1
+    let res = 
+        if (not loop_break) then
+            List.append lst_copied [prime_factor(factor, exp)]
+        else
+            lst_copied
+    res
+
+printfn "Data Initialization Done!\n"
+
+let prime_gen_sw = new Stopwatch()
+prime_gen_sw.Start()
+prime_gen prime_num_insert prime_set_end prime_set
+prime_gen_sw.Stop()
+
+printfn "Prime generation to %d Done! (Elapsed Time : %fms)\n" prime_num_insert prime_gen_sw.Elapsed.TotalMilliseconds
+
+/// find_prime_factors : given an integer, find its prime_factors and returns them as prime_factor list
 let find_prime_factors (n : int) =
-    let sqrt_n = Convert.ToInt32(System.Math.Sqrt(float n))
+    //let sqrt_n = Convert.ToInt32(System.Math.Sqrt((float n)))
+    let sqrt_n = n/2 + 1
     let mutable num = n
     let mutable cnt = 0
     let mutable loop_break = false
-    let ans = new HashSet<prime_factor>()
-    while (num <> 1)&&(not loop_break)&&(cnt < prime_set.Count) do
+    let ans = ref ([] : prime_factor list)
+    while (num > 1)&&(not loop_break)&&(cnt < prime_set.Count) do
         let elem = prime_set.ElementAt(cnt)
-        let mutable exp_cnt = 0
-        if num%elem = 0 then
-            while num%elem=0 do
-                num <- num/elem
-                exp_cnt <- exp_cnt + 1
-            ans.Add(prime_factor(elem, exp_cnt)) |> ignore
+        let mutable exp = 0
+        while (num > 1)&&(num%elem = 0) do
+            num <- num/elem
+            exp <- exp + 1
+        if factored_set.ContainsKey(num) then
+            let prev_pf = unbox (factored_set.Item(num))
+            ans.Value <- prime_factor_adder elem exp prev_pf
+            loop_break <- true
+        else
+            if exp <> 0 then
+                ans.Value <- List.append ans.Value [prime_factor(elem, exp)]
+            //if (prime_set.ElementAt(cnt + 1) > sqrt_n)||(num=1) then
+            if (elem > sqrt_n) || (num=1) then
+                loop_break <- true
         cnt <- cnt + 1
-    //factored_set.Add(n, ans)
-    List.ofSeq(ans)
+    if ans.Value.Length = 0 then
+        ans.Value <- [prime_factor(n, 1)]
+    factored_set.Add(n, ans.Value)
+    ans.Value
 
-let find_ans (n : int) =
-    (*if prime_set_end.Value < n then
-        prime_gen (n+1) prime_set_end prime_set*)
-    let set1 = ref ([] : prime_factor list)
-    let set2 = ref ([] : prime_factor list)
-    let set3 = ref ([] : prime_factor list)
-    let set4 = ref ([] : prime_factor list)
-    for i=2 to n-2 do
-        printfn "finding answer : i = %d" i
-        set1.Value <- set2.Value
-        set2.Value <- set3.Value
-        set3.Value <- set4.Value
-        set4.Value <- find_prime_factors(i)
-        let set1_cnt = set1.Value.Length
-        let set2_cnt = set2.Value.Length
-        let set3_cnt = set3.Value.Length
-        let set4_cnt = set4.Value.Length
-        if set1_cnt=4 then
-            if set2_cnt=4 then 
-                if set3_cnt=4 then
-                    if set4_cnt=4 then
-                        let set_app = List.append (List.append (List.append set1.Value set2.Value) set3.Value) set4.Value
-                        let num_dist = set_app.Distinct().Count()
-                        if set1_cnt+set2_cnt+set3_cnt+set4_cnt = num_dist then
-                            printfn "Found three consecutive numbers to have three distinct prime factors : "
-                            printf "%d : " i
-                            for elem in set1.Value do
-                                printf "%s " (elem.ToString())
-                            printf "\n%d : " (i+1)
-                            for elem in set2.Value do  
-                                printf "%s " (elem.ToString())
-                            printf "\n%d : " (i+2)
-                            for elem in set3.Value do  
-                                printf "%s " (elem.ToString())
-                            printf "\n%d : " (i+3)
-                            for elem in set4.Value do  
-                                printf "%s " (elem.ToString())
-                            printfn "\n"
-                            File.WriteAllText("res.txt", i.ToString())
-        
-let mutable max_int_ans = -1
-printfn "Enter the maximum natural number to search for answer :"
-max_int_ans <- Convert.ToInt32(Console.ReadLine())
-prime_gen max_int_ans prime_set_end prime_set
-printfn "Prime gen done!"
-find_ans(max_int_ans)
-
-(*prime_gen 100000 prime_set_end prime_set
-printfn "Prime factors of 120 is : "
-let pf_24 = find_prime_factors(120)
-for elem in pf_24 do
-    printfn "%s" (elem.ToString())*)
+let prime_factor_sw = new Stopwatch()
+prime_factor_sw.Start()
+let mutable error_count = 0
+for i=2 to prime_factor_insert do
+    //find_prime_factors i |> ignore
+    let res = find_prime_factors i
+    (*printf "Prime factors of %d is : " i
+    print_list res*)
+    let pf_check = (check_prime_factor i res)
+    if (not pf_check) then
+        error_count <- error_count + 1
+    //printfn "Check correctness : %s" (pf_check.ToString())
+prime_factor_sw.Stop()
+printfn "Prime factor generation Done! (Elapsed Time : %fms) (Error : %d)\n" prime_factor_sw.Elapsed.TotalMilliseconds error_count
--- prob47/prob47/prime_funcs.fs
+++ prob47/prob47/prime_funcs.fs
@@ -8,36 +8,36 @@ open System.IO
 open System.Linq
 open System.Numerics
 
+type prime_factor(basis : int, exp : int) =
+    member val basis = basis with get, set
+    member val exp = exp with get, set
+    override pf.ToString() = basis.ToString() + "^" + exp.ToString()
+    
+
 /// prime_gen : This function generates primes less than or equal to n, 
 /// sets prime_set_end to n, and saves the generated primes to prime_set
 let prime_gen (n : int) (prime_set_end : int ref) (prime_set : HashSet<int>) =
     if n > prime_set_end.Value then
         for i=prime_set_end.Value+1 to n do
-            printfn "Prime_gen : i = %d" i
             if i=2 then
                 prime_set.Add(i) |> ignore
-            if i=3 then
-                prime_set.Add(i) |> ignore
-            if i=5 then
-                prime_set.Add(i) |> ignore
+                //printfn "Generated prime : %d" i
             else if i%2<>0 && i > 2 then
-                if i%3 <> 0 then
-                    if i%5 <> 0 then
-                        let sqrt_i = Convert.ToInt32(System.Math.Sqrt(float i))
-                        let mutable loop_break = false
-                        let mutable cnt = 0
-                        let mutable res = true
-                        while (not loop_break) && (cnt < prime_set.Count) do
-                            let elem = prime_set.ElementAt(cnt)
-                            if elem > sqrt_i then
-                                loop_break <- true
-                            else
-                                if i%elem=0 then
-                                    res <- false
-                                    loop_break <- true
-                                else
-                                    res <- res
-                                    cnt <- cnt + 1
-                        if res then
-                            prime_set.Add(i) |> ignore
+                let sqrt_i = Convert.ToInt32(System.Math.Sqrt(float i))
+                let mutable loop_break = false
+                let mutable cnt = 0
+                let mutable res = true
+                while (not loop_break) && (cnt < prime_set.Count) do
+                    let elem = prime_set.ElementAt(cnt)
+                    if i%elem=0 then
+                        res <- false
+                        loop_break <- true
+                    else if elem > sqrt_i then
+                        loop_break <- true
+                    else
+                        res <- res
+                        cnt <- cnt + 1
+                if res then
+                    prime_set.Add(i) |> ignore
+                    //printfn "Generated prime : %d" i
     prime_set_end.Value <- n
