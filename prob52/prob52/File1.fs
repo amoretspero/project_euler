b3ec974c72ee807a30a8d366c7317f960facce7c
Jiung Hahm <amoretspero@gmail.com>
2015-08-29 17:08:04 +0900

    Solved prob52, 53 - Comments needed.

---
+++ prob52/prob52/File1.fs
@@ -0,0 +1,70 @@
+﻿module File1
+
+open System
+open System.Collections
+open System.Collections.Generic
+open System.Diagnostics
+open System.IO
+open System.Linq
+open System.Numerics
+
+let num_to_digit (n : int) =
+    List.map (fun x -> int(x.ToString())) (List.ofArray(n.ToString().ToCharArray()))
+
+let rec digit_compare (lst1 : int list) (lst2 : int list) =
+    match lst1 with
+    | [] ->
+        match lst2 with
+        | [] -> true
+        | _ -> false
+    | h1 :: t1 ->
+        match lst2 with
+        | [] -> false
+        | h2 :: t2 ->
+            if h1 = h2 then
+                digit_compare t1 t2
+            else
+                false
+
+
+let check_ans (mult : int) (limit : int) =
+    let mutable cnt = 1
+    let mutable loop_break = false
+    while (not loop_break) && (cnt < limit) do
+        let mutable multiple = cnt
+        let mutable mult_cnt = 0
+        let mutable mult_loop_break = false
+        let digit_lst = ref (List.sort(num_to_digit multiple))
+        while mult_cnt < mult && (not mult_loop_break) do
+            let temp = List.sort(num_to_digit multiple)
+            if (not (digit_compare digit_lst.Value temp)) then
+                mult_loop_break <- true
+            else
+                mult_cnt <- mult_cnt + 1
+                multiple <- multiple + cnt
+        if mult_loop_break then
+            cnt <- cnt + 1
+        else
+            printfn "Found answer! : x = %d" cnt
+            loop_break <- true
+    cnt
+
+let mutable number_of_multiples = -1
+let mutable check_limit = -1
+printfn "Insert number_of_multiples :"
+number_of_multiples <- System.Convert.ToInt32(System.Console.ReadLine())
+printfn "Insert check_limit :"
+check_limit <- System.Convert.ToInt32(System.Console.ReadLine())
+
+let find_ans_sw = new Stopwatch()
+find_ans_sw.Start()
+let ans = check_ans number_of_multiples check_limit
+find_ans_sw.Stop()
+
+printfn "\nSearch for answer for multiple to %d and to limit of %d done. (Elapsed Time : %fms)" number_of_multiples check_limit find_ans_sw.Elapsed.TotalMilliseconds
+
+(*
+===================
+Answer : 142857
+===================
+*)
---
+++ prob53/prob53/File1.fs
@@ -0,0 +1,62 @@
+﻿module File1
+
+open System
+open System.Collections
+open System.Collections.Generic
+open System.Diagnostics
+open System.IO
+open System.Linq
+open System.Numerics
+
+exception InvalidInput
+
+/// Computes nCr
+let combination (n : BigInteger) (r : BigInteger) =
+    if n < r || n <= 0I || r < 0I then
+        raise InvalidInput
+    else
+        let mutable res = 1I
+        let n_temp = (int n)
+        let r_temp = if r > n/2I then (int (n-r)) else (int r)
+        for i=n_temp downto n_temp - r_temp + 1 do
+            res <- res * System.Numerics.BigInteger.Parse(i.ToString())
+        for j=1 to r_temp do
+            res <- res / System.Numerics.BigInteger.Parse(j.ToString())
+        res
+
+/// Returns number of combinations larger than limit with n between n_start and n_end inclusive. 
+let combination_determine (limit : BigInteger) (n_start : int) (n_end : int) =
+    let mutable cnt = 0
+    for n=n_start to n_end do
+        for r=0 to n do
+            let temp = combination (BigInteger.Parse(n.ToString())) (BigInteger.Parse(r.ToString()))
+            if temp > limit then
+                cnt <- cnt + 1
+    cnt
+
+let mutable limit_input = -1I
+let mutable n_start_input = -1
+let mutable n_end_input = -1
+
+printfn "Insert limit_input :"
+limit_input <- BigInteger.Parse(System.Console.ReadLine())
+printfn ""
+printfn "Insert n_start_input :"
+n_start_input <- System.Convert.ToInt32(System.Console.ReadLine())
+printfn ""
+printfn "Insert n_end_input :"
+n_end_input <- System.Convert.ToInt32(System.Console.ReadLine())
+printfn ""
+
+let find_ans_sw = new Stopwatch()
+printfn "Now starting calculation...\n"
+find_ans_sw.Start()
+let ans = combination_determine limit_input n_start_input n_end_input
+find_ans_sw.Stop()
+printfn "Calculation done. Found %d combinations exceeds %s (Elapsed Time : %fms)" ans (limit_input.ToString()) find_ans_sw.Elapsed.TotalMilliseconds
+
+(*
+===================
+Answer :4075
+===================
+*)
